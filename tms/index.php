f280efd2ccc3ce6ca535eb019316a6d2fde1b5d2
tsetan <tsetanjingmal@gmail.com>
2022-03-21 01:47:49 +0530

    Update index.php

--- tms/index.php
+++ tms/index.php
@@ -26,7 +26,7 @@ include('includes/config.php');
 <?php include('includes/header.php');?>
 <div class="banner">
 	<div class="container">
-		<h1 class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> LADAKH MOUNTAINS CALLING</h1>
+		
 	</div>
 </div>
 
